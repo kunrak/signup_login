135a0698038a4c9e3d75491812168fea53ba1fd7
kunrak <rakeshkunducsgi@gmail.com>
2023-05-03 13:52:09 +0530

    update

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -1,10 +1,19 @@
-import { Outlet, Navigate } from 'react-router-dom'
+// import { Outlet, Navigate } from 'react-router-dom'
 
-const ProtectedRoutes = () => {
-    const isAuthenticated = !!localStorage.getItem("myData");
-    return (
-        isAuthenticated ? <Outlet /> : <Navigate to="/profile" />
-    )
+// const ProtectedRoutes = () => {
+//     const isAuthenticated = !!localStorage.getItem("myData");
+//     return (
+//         isAuthenticated ? <Outlet /> : <Navigate to="/profile" />
+//     )
+// }
+
+// export default ProtectedRoutes
+
+import { Navigate, Outlet } from "react-router-dom"
+
+export const ProtectedRoute = ({ isAllowed, redirectPath = "/login", children }) => {
+    if (!isAllowed) {
+        return <Navigate to={redirectPath} replace />
+    }
+    return children ? children : <Outlet />
 }
-
-export default ProtectedRoutes
